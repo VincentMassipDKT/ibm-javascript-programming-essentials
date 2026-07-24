e0de584824dcc5e306e8fda7d7e86659e77f1cb4
VincentMassipDKT <vincent.massip@decathlon.com>
2026-07-24 18:45:44 +0200

    Addind Employee Details project to repo

--- restaurant-menu/restaurant-menu.js
+++ restaurant-menu/restaurant-menu.js
@@ -1,8 +1,9 @@
 const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const breakfastMenuWithPrices = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 
-const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+const breakfastMenuItemsHTML = breakfastMenuWithPrices.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
 
