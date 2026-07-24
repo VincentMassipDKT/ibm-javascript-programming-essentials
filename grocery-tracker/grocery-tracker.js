499a07c5a5063733fe3586f4d4b76bb09e9117ea
VincentMassipDKT <vincent.massip@decathlon.com>
2026-07-24 12:47:29 +0200

    Adding Grocery Tracker project to the repo for Practice task step

---
+++ grocery-tracker/grocery-tracker.js
@@ -0,0 +1,9 @@
+function calculateTotalAmount() {
+    let grocery1 = parseFloat(document.getElementById('grocery1').value);
+    let grocery2 = parseFloat(document.getElementById('grocery2').value);
+    let grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    let groceryTotal = grocery1 + grocery2 + grocery3;
+    
+    document.getElementById('result').innerText = `The total amount is: $${groceryTotal}`;
+};
