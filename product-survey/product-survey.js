192f8da68ca64fc1b89156a1d8f9e82cfd504677
VincentMassipDKT <vincent.massip@decathlon.com>
2026-07-24 17:20:38 +0200

    Practice task for Develop Product Feedback Survey Form Using Events hands-on lab

--- product-survey/product-survey.js
+++ product-survey/product-survey.js
@@ -6,6 +6,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userExperience = document.getElementById('userExperienceText').value;
 
     alert('Thank you for your valuable feedback');
 
@@ -16,12 +17,15 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = userExperience;
 
     document.getElementById('userInfo').style.display = 'block';
 }
 
+
 const submitButton = document.getElementById('submitBtn');
 submitButton.onclick = submitFeedback;
+
 
 document.addEventListener('keydown', function(event) {
    if (event.key === 'Enter') {
