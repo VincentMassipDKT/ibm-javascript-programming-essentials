9f3319aa420048b81424d8e79606ccbbaba0fe09
VincentMassipDKT <vincent.massip@decathlon.com>
2026-07-27 17:46:06 +0200

    Practice task for Debug a JavaScript Application in Chrome Dev Tools hands-on lab

--- debug-code/debug-code.js
+++ debug-code/debug-code.js
@@ -1,33 +1,101 @@
 
-function performOperation() {
-    // Get user input from input fields
-    let num1 = parseInt(document.getElementById('input1').value);
-    let num2 = parseInt(document.getElementById('input2').value);
-    // Check if inputs are valid numbers
-    if (!isNaN(num1) && !isNaN(num2)) {
-        // Perform the operation
-        let result = multiply(num1, num2);
-
-        // Display the result
-        displayResult(result);
-    } else {
-        displayResult('Please enter valid numbers');
-    }
-}
-
-
-function multiply(a, b) {
+function performOperations() {
     // Introduce a debugger statement to pause execution
     debugger;
 
-    // Multiply the numbers
+    // Get user input from input fields
+    let num1 = parseInt(document.getElementById('input1').value);
+    let num2 = parseInt(document.getElementById('input2').value);
+
+    // Check if inputs are valid numbers
+    if (!isNaN(num1) && !isNaN(num2)) {
+
+        // Perform the operations
+        let results = []
+        results.push(add(num1, num2));
+        results.push(substract(num1, num2));
+        results.push(multiply(num1, num2));
+        results.push(divide(num1, num2));
+        results.push(divisionRemainder(num1, num2));
+
+        // Display the result
+        displayResults(results);
+    } else {
+        displayError('⚠ Please enter valid numbers ⚠');
+    }
+}
+
+/**
+ * Add the numbers
+ * @param {Number} a 
+ * @param {Number} b 
+ * @returns {Number}
+ */
+function add(a, b) {
+    return a + b;
+}
+
+/**
+ * Substract the numbers
+ * @param {Number} a 
+ * @param {Number} b 
+ * @returns {Number}
+ */
+function substract(a, b) {
+    return a - b;
+}
+
+/**
+ * Multiply the numbers
+ * @param {Number} a 
+ * @param {Number} b 
+ * @returns {Number}
+ */
+function multiply(a, b) {
     return a * b;
 }
 
+/**
+ * Divide the numbers
+ * @param {Number} a 
+ * @param {Number} b 
+ * @returns {Number}
+ */
+function divide(a, b) {
+    return a / b;
+}
 
-function displayResult(result) {
-    // Display the result in the paragraph element
+
+/**
+ * Divide the numbers and gets the remainder
+ * @param {Number} a 
+ * @param {Number} b 
+ * @returns {Number}
+ */
+function divisionRemainder(a, b) {
+    return a % b;
+}
+
+/**
+ * Display the result in the paragraph element
+ * @param {Array<Number>} results 
+ */
+function displayResults(results) {
     const resultElement = document.getElementById('result');
-    resultElement.textContent = `The result is: ${result}`;
+    resultElement.innerText = `The result are: 
+    - Addition: ${results[0]}
+    - Substraction: ${results[1]}
+    - Multiplication: ${results[2]}
+    - Division: ${results[3]}
+    - Division Remainder: ${results[4]}`;
+}
+
+/**
+ * Display an error message instead of the results
+ * @param {String} errorMessage
+ */
+function displayError(errorMessage) {
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = errorMessage;
 }
 
