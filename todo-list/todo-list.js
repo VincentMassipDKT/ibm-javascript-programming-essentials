b0034f5ff05f9342b4d5000f50ab9eceaad4c448
VincentMassipDKT <vincent.massip@decathlon.com>
2026-07-28 12:03:06 +0200

    Practice task for Create a To-Do List Using JavaScript hands-on lab

--- todo-list/todo-list.js
+++ todo-list/todo-list.js
@@ -3,6 +3,7 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAllBtn = document.getElementById("clearAllBtn");
 
 let tasks = [];
 
@@ -41,6 +42,12 @@ function clearCompletedTasks() {
 }
 
 
+function clearAllTasks() {
+    tasks.length = 0;
+    displayTasks();
+}
+
+
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
-
+clearAllBtn.addEventListener("click", clearAllTasks);
